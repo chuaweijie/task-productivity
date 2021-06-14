2143db26edea56cadb10c138ce91171b477b98bc
chuaweijie <chuawei_jie@hotmail.com>
2021-06-14 18:00:02 +0700

    Updated the static settings as refered from the Heroku documentation.

--- capstone/settings.py
+++ capstone/settings.py
@@ -112,10 +112,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = PurePosixPath(BASE_DIR).joinpath('static')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
