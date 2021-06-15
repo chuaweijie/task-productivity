122ca644c3c289b233fa6cfb18d5192014884f1a
chuaweijie <chuawei_jie@hotmail.com>
2021-06-15 22:10:59 +0700

    Updated the settings file to solve the 500 error.

--- capstone/settings.py
+++ capstone/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
