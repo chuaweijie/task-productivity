399f67aadf5b7755c6b209396b570ba67d3836fe
Chua Wei Jie <chuawei_jie@hotmail.com>
2021-06-18 14:44:56 +0700

    Added login, signup and restructured files.
    1. Added login.html and signup.html. 2. Modified index and layout. 3. Modified urls and views. 4. Changed the picture from taskproductivity to img.

--- taskproductivity/urls.py
+++ taskproductivity/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.login, name="login"),
-    path("", views.signup, name="signup"),
+    path("login", views.login, name="login"),
+    path("signup", views.signup, name="signup"),
 ]
--- taskproductivity/views.py
+++ taskproductivity/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    # How to check if the user is already logged in? 
     return render(request, "taskproductivity/index.html")
 
 def login(request):
-    return render(request, "taskproductivity/index.html")
+    
+    return render(request, "taskproductivity/login.html")
 
 def signup(request):
-    return render(request, "taskproductivity/index.html")
+    return render(request, "taskproductivity/signup.html")
