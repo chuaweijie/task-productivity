74f7d33dbba772b21085174f0d8f65adf46e4f37
chuaweijie <chuawei_jie@hotmail.com>
2021-09-24 13:03:59 +0700

    Removed the version from requirements and the conn_max_age from local_settings.py

--- capstone/local_settings.py
+++ capstone/local_settings.py
@@ -1,4 +1,4 @@
 DATABASES = {}
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(engine='django.db.backends.postgresql', conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(engine='django.db.backends.postgresql')
